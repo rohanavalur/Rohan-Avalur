fd30a05c95e577b0c08ea85c3da81443cc17eeb1
rohanavalur <122695107+rohanavalur@users.noreply.github.com>
2024-10-12 17:28:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// script.js
+// Hide preloader when the page has fully loaded
 window.addEventListener('load', function () {
-    const preloader = document.getElementById('preloader');
+    const preloader = document.querySelector('.preloader');
     preloader.style.display = 'none';
 });
